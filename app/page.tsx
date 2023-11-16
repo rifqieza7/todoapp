ef3f170f221a735201391e07e72aea605a32f869
eza rifqi <ezarifqi@gmail.com>
2023-11-16 16:45:57 +0700

    fix delete todos

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import TodoList from "./components/TodoList";
 
 export default async function Home() {
   const tasks = await getAllTodos();
-  console.log(tasks);
+
   return (
-    <main className="max-w-4xl mx-auto mt-4">
-      <div className="text-center my-5 flex flex-col gap-4 mr">
-        <h1 className="text-2xl font-bold text-yellow-500">Todo List App</h1>
+    <main className='max-w-4xl mx-auto mt-4'>
+      <div className='text-center my-5 flex flex-col gap-4'>
+        <h1 className='text-2xl font-bold'>Todo List App</h1>
         <AddTask />
       </div>
-      <TodoList tasks={tasks}/>
+      <TodoList tasks={tasks} />
     </main>
   );
-  }
+}
